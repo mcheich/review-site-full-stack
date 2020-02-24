cc01e1985b6f35a39aa102e99bebcd3d03f1fe8a
mcheich <mcheich@gmail.com>
2020-02-24 18:56:09 -0500

    Test drove dev of categories and tags in review controller

--- src/main/java/com/reviewSite/fullStack/ReviewController.java
+++ src/main/java/com/reviewSite/fullStack/ReviewController.java
@@ -10,25 +10,61 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReviewController {
-	
+
 	@Resource
 	private ReviewRepository reviewRepo;
 
-	
+	@Resource
+	private CategoryRepository categoryRepo;
+
+	@Resource
+	private TagRepository tagRepo;
+
 	public void findOneReview(long id, Model model) {
-		
+
 		Optional<Review> review = reviewRepo.findById(id);
-		
-		if(review.isPresent()) {
+
+		if (review.isPresent()) {
 			model.addAttribute("review", review.get());
+		}
+
+	}
+
+	public void findAllReviews(Model model) {
+
+		model.addAttribute("reviews", reviewRepo.findAll());
+
+	}
+
+	public void findOneCategory(long id, Model model) {
+
+		Optional<Category> category = categoryRepo.findById(id);
+
+		if (category.isPresent()) {
+			model.addAttribute("category", category.get());
+		}
+
+	}
+
+	public void findAllCategories(Model model) {
+
+		model.addAttribute("categories", categoryRepo.findAll());
+
+	}
+
+	public void findOneTag(long id, Model model) {
+		
+		Optional<Tag> tag = tagRepo.findById(id);
+
+		if (tag.isPresent()) {
+			model.addAttribute("tag", tag.get());
 		}
 		
 	}
 
-
-	public void findAllReviews(Model model) {
+	public void findAllTags(Model model) {
 		
-		model.addAttribute("reviews", reviewRepo.findAll());
+		model.addAttribute("tags", tagRepo.findAll());
 		
 	}
 
--- src/test/java/com/reviewSite/fullStack/ReviewsControllerTest.java
+++ src/test/java/com/reviewSite/fullStack/ReviewsControllerTest.java
@@ -36,6 +36,24 @@ public class ReviewsControllerTest {
 
 	@Mock
 	private Review reviewTwo;
+
+	@Mock
+	private CategoryRepository categoryRepo;
+	
+	@Mock
+	private Category categoryOne;
+	
+	@Mock
+	private Category categoryTwo;
+
+	@Mock
+	private TagRepository tagRepo;
+	
+	@Mock
+	private Tag tagOne;
+	
+	@Mock
+	private Tag tagTwo;
 	
 	@Before
 	public void setUp() {
@@ -70,7 +88,54 @@ public class ReviewsControllerTest {
 	
 	@Test
 	public void shouldAddSingleCategoryToModel() {
+		//Arrange
+		long id = 1;
+		when(categoryRepo.findById(id)).thenReturn(Optional.of(categoryOne));
 		
+		//Act
+		underTest.findOneCategory(id, model);
+		
+		//Assert
+		verify(model).addAttribute("category", categoryOne);
+	}
+	
+	@Test
+	public void shouldAddAllCategoriesToModel() {
+		//Arrange
+		Collection<Category> allCategories = Arrays.asList(categoryOne, categoryTwo);
+		when(categoryRepo.findAll()).thenReturn(allCategories);
+		
+		//Act
+		underTest.findAllCategories(model);
+		
+		//Assert
+		verify(model).addAttribute("categories", allCategories);
+	}
+	
+	@Test
+	public void shouldAddSingleTagToModel() {
+		//Arrange
+		long id = 1;
+		when(tagRepo.findById(id)).thenReturn(Optional.of(tagOne));
+		
+		//Act
+		underTest.findOneTag(id, model);
+		
+		//Assert
+		verify(model).addAttribute("tag", tagOne);
+	}
+	
+	@Test
+	public void shouldAddAllTagsToModel() {
+		//Arrange
+		Collection<Tag> allTags = Arrays.asList(tagOne, tagTwo);
+		when(tagRepo.findAll()).thenReturn(allTags);
+		
+		//Act
+		underTest.findAllTags(model);
+		
+		//Assert
+		verify(model).addAttribute("tags", allTags);
 	}
 
 }
