ca7372e7eaf7b5f571e510fac05fdebe123af4ae
mcheich <mcheich@gmail.com>
2020-02-25 09:38:36 -0500

    added test for finding reveiws given a category id

--- src/test/java/com/reviewSite/fullStack/ReviewControllerMockMvcTest.java
+++ src/test/java/com/reviewSite/fullStack/ReviewControllerMockMvcTest.java
@@ -1,9 +1,15 @@
 package com.reviewSite.fullStack;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -34,11 +40,24 @@ class ReviewControllerMockMvcTest {
 	@Mock
 	private Review reviewOne;
 	
-	/********* All Course Tests Follow ***********/
+
 	@Test
 	public void shouldGetStatusOfOkWhenWhenNavigatingToAllReviewsOfCategory() throws Exception {
+		when(reviewRepo.findById(1L)).thenReturn(Optional.of(reviewOne));
 		mvc.perform(get("/categories?id=1")).andExpect(status().isOk())
 				.andExpect(view().name("category"));
+	}
+	
+	
+	@Test
+	public void shouldAddSingleReviewToTheModel() throws Exception {
+		when(reviewRepo.findById(1L)).thenReturn(Optional.of(reviewOne));
+		mvc.perform(get("/categories?id=1")).andExpect(model().attribute("review", is(reviewOne)));
+	}
+	
+	@Test
+	public void shouldAllReviewsOfACategoryToTheModel() throws Exception {
+		//when(reviewRepo.findByCategoryId(categoryId))
 	}
 
 }
--- src/test/java/com/reviewSite/fullStack/ReviewControllerTest.java
+++ src/test/java/com/reviewSite/fullStack/ReviewControllerTest.java
@@ -19,18 +19,18 @@ public class ReviewControllerTest {
 
 	@Mock
 	private Model model;
-	
-	/* Mikes Note to self:
-	 * Ask about this annotation in class.
-	 * Why InjectMocks vs just a Mock?
+
+	/*
+	 * Mikes Note to self: Ask about this annotation in class. Why InjectMocks vs
+	 * just a Mock?
 	 **/
-	
+
 	@InjectMocks
 	private ReviewController underTest;
-	
+
 	@Mock
 	private ReviewRepository reviewRepo;
-	
+
 	@Mock
 	private Review reviewOne;
 
@@ -39,102 +39,110 @@ public class ReviewControllerTest {
 
 	@Mock
 	private CategoryRepository categoryRepo;
-	
+
 	@Mock
 	private Category categoryOne;
-	
+
 	@Mock
 	private Category categoryTwo;
 
 	@Mock
 	private TagRepository tagRepo;
-	
+
 	@Mock
 	private Tag tagOne;
-	
+
 	@Mock
 	private Tag tagTwo;
-	
+
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
 	public void shouldAddSingleReviewToModel() throws ReviewNotFoundException {
-		//Arrange
+		// Arrange
 		long id = 1;
 		when(reviewRepo.findById(id)).thenReturn(Optional.of(reviewOne));
-		
-		//Act
+
+		// Act
 		underTest.findOneReview(id, model);
-		
-		//Assert
+
+		// Assert
 		verify(model).addAttribute("review", reviewOne);
 	}
-	
+
 	@Test
 	public void shouldAddAllReviewsToModel() {
-		//Arrange
+		// Arrange
 		Collection<Review> allReviews = Arrays.asList(reviewOne, reviewTwo);
 		when(reviewRepo.findAll()).thenReturn(allReviews);
-		
-		//Act
+
+		// Act
 		underTest.findAllReviews(model);
-		
-		//Assert
+
+		// Assert
 		verify(model).addAttribute("reviews", allReviews);
 	}
-	
+
+	@Test
+	public void shouldAddAllReviewsForCategoryToModel() {
+		// Arrange
+		Collection<Review> allReviews = Arrays.asList(reviewOne, reviewTwo);
+		
+		
+	}
+
 	@Test
 	public void shouldAddSingleCategoryToModel() {
-		//Arrange
+		// Arrange
 		long id = 1;
 		when(categoryRepo.findById(id)).thenReturn(Optional.of(categoryOne));
-		
-		//Act
+
+		// Act
 		underTest.findOneCategory(id, model);
-		
-		//Assert
+
+		// Assert
 		verify(model).addAttribute("category", categoryOne);
 	}
-	
+
 	@Test
 	public void shouldAddAllCategoriesToModel() {
-		//Arrange
+		// Arrange
 		Collection<Category> allCategories = Arrays.asList(categoryOne, categoryTwo);
 		when(categoryRepo.findAll()).thenReturn(allCategories);
-		
-		//Act
+
+		// Act
 		underTest.findAllCategories(model);
-		
-		//Assert
+
+		// Assert
 		verify(model).addAttribute("categories", allCategories);
 	}
-	
+
 	@Test
 	public void shouldAddSingleTagToModel() {
-		//Arrange
+		// Arrange
 		long id = 1;
 		when(tagRepo.findById(id)).thenReturn(Optional.of(tagOne));
-		
-		//Act
+
+		// Act
 		underTest.findOneTag(id, model);
-		
-		//Assert
+
+		// Assert
 		verify(model).addAttribute("tag", tagOne);
 	}
-	
+
 	@Test
 	public void shouldAddAllTagsToModel() {
-		//Arrange
+		// Arrange
 		Collection<Tag> allTags = Arrays.asList(tagOne, tagTwo);
 		when(tagRepo.findAll()).thenReturn(allTags);
-		
-		//Act
+
+		// Act
 		underTest.findAllTags(model);
-		
-		//Assert
+
+		// Assert
 		verify(model).addAttribute("tags", allTags);
 	}
 
